e40b17ef3cb809b1f169e26b112ca59ffb6af353
shunkie <shunkie@qq.com>
2019-02-07 02:25:21 +0800

    install sslify

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const path = require('path')
+const http = require('http')
 const https = require('https')
 const Koa = require('koa')
 const mount = require('koa-mount')
 const serve = require('koa-static')
+const { default: enforceHttps } = require('koa-sslify')
 const mongoose = require('mongoose')
 const config = require('./config')
 
@@ -15,16 +17,15 @@ mongoose.connect(config.mongodb.url, {
 const app = new Koa()
 const router = require('./router')
 
+app.use(enforceHttps())
 app.use(mount('/image', serve(__dirname + '/public/image')))
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-
 const options = {
-    key: fs.readFileSync(path.join(__dirname, 'cert/mine.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert/mine.pem'))
-  }
+  key: fs.readFileSync(path.join(__dirname, 'cert/mine.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert/mine.pem'))
+}
 
-https.createServer(options, app.callback()).listen(80, () => {
-  console.log('server is running...')
-})
+http.createServer(app.callback()).listen(80)
+https.createServer(options, app.callback()).listen(443)
